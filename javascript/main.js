b4705273c18637918aba2198a1f96b480c6534db
Kadievka <kadievka.s.ks@gmail.com>
2020-01-24 13:59:28 -0400

    remover una clse en un elemento y agregar otra

---
+++ javascript/main.js
@@ -0,0 +1,12 @@
+let links = document.querySelectorAll(".close");
+links.forEach(function (link) {
+    link.addEventListener("click", function (ev) {
+        ev.preventDefault();
+        let content = document.querySelector(".content");
+        content.classList.remove("fadeInDown");
+        content.classList.remove("animated");
+        content.classList.add("fadeOutUp");
+        content.classList.add("animated");
+        return false;
+    });
+});
