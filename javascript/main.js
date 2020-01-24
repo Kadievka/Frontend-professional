b4e6eb57fb0c4325a7d91783b97646d126cf53a3
Kadievka <kadievka.s.ks@gmail.com>
2020-01-24 14:44:19 -0400

    de nuevo correción

--- javascript/main.js
+++ javascript/main.js
@@ -9,7 +9,7 @@ links.forEach(function (link) {
         content.classList.add("animated");
 
         setTimeout(function () {
-            location.href = "../";
+            location.href = "../index.html";
         }, 600);
 
         return false;
